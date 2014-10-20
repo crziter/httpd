1b7c2885f8b609a69457a0d4b806b15aaece2d06
Bình Nguyên <crziter@gmail.com>
2014-10-21 04:00:33 +0700

    tcp_socket kế thừa từ socket_interface

--- httpd/tcp_socket.cpp
+++ httpd/tcp_socket.cpp
@@ -21,54 +21,101 @@ void tcp_socket::close()
     _remote_ip = std::string();
 }
 
-const std::string& tcp_socket::remote_ip()
+// std::string& tcp_socket::remote_ip()
+// {
+//     throw std::logic_error("The method or operation is not implemented.");
+// }
+// 
+// ushort& tcp_socket::remote_port()
+// {
+//     throw std::logic_error("The method or operation is not implemented.");
+// }
+
+int tcp_socket::send(char_ptr buff, int lenth)
+{
+    return ::send(_sock, buff, lenth, 0);
+}
+
+int tcp_socket::recv(char_ptr buff, int length)
+{
+    return ::recv(_sock, buff, length, 0);
+}
+
+std::string& tcp_socket::remote_ip()
 {
     return _remote_ip;
 }
 
-const ushort tcp_socket::remote_port()
+ushort& tcp_socket::remote_port()
 {
     return _remote_port;
 }
 
-int tcp_socket::send(const char_ptr buffer, int len, int pos /*= 0*/)
-{
-    return ::send(_sock, buffer + pos, len, 0);
-}
 
-int tcp_socket::send(const std::string& msg)
-{
-    return this->send((const char_ptr)msg.c_str(), msg.length(), 0);
-}
 
-int tcp_socket::recv(char *buffer, int len, int pos /*= 0*/)
-{
-    return ::recv(_sock, buffer + pos, len, 0);
-}
 
-int tcp_socket::recv(std::string& msg)
-{
-    msg.clear();
-    char buffer[1024 * 3];
-    int len = this->recv(buffer, sizeof(buffer), 0);
 
-    if (len > 0)
-        msg.append(buffer, len);
 
-    return len;
-}
-
-int tcp_socket::recv(vector_char& buffer)
-{
-    buffer.clear();
-
-    char buff[1024 * 3];
-    int len = this->recv(buff, sizeof(buff), 0);
-
-    if (len > 0) {
-        buffer.resize(sizeof(buff));
-        buffer.assign(buff, buff + len);
-    }
-
-    return len;
-}
+// 
+// const std::string& tcp_socket::remote_ip()
+// {
+//     return _remote_ip;
+// }
+// 
+// const ushort tcp_socket::remote_port()
+// {
+//     return _remote_port;
+// }
+// 
+// int tcp_socket::send(const char_ptr buffer, int len, int pos /*= 0*/)
+// {
+//     return ::send(_sock, buffer + pos, len, 0);
+// }
+// 
+// int tcp_socket::send(const std::string& msg)
+// {
+//     return this->send((const char_ptr)msg.c_str(), msg.length(), 0);
+// }
+// 
+// int tcp_socket::send(char_ptr buff, int lenth)
+// {
+//     throw std::logic_error("The method or operation is not implemented.");
+// }
+// 
+// 
+// int tcp_socket::recv(char *buffer, int len, int pos /*= 0*/)
+// {
+//     return ::recv(_sock, buffer + pos, len, 0);
+// }
+// 
+// int tcp_socket::recv(std::string& msg)
+// {
+//     msg.clear();
+//     char buffer[1024 * 3];
+//     int len = this->recv(buffer, sizeof(buffer), 0);
+// 
+//     if (len > 0)
+//         msg.append(buffer, len);
+// 
+//     return len;
+// }
+// 
+// int tcp_socket::recv(vector_char& buffer)
+// {
+//     buffer.clear();
+// 
+//     char buff[1024 * 3];
+//     int len = this->recv(buff, sizeof(buff), 0);
+// 
+//     if (len > 0) {
+//         buffer.resize(sizeof(buff));
+//         buffer.assign(buff, buff + len);
+//     }
+// 
+//     return len;
+// }
+// 
+// int tcp_socket::recv(char_ptr buff, int length)
+// {
+//     throw std::logic_error("The method or operation is not implemented.");
+// }
--- httpd/tcp_socket.h
+++ httpd/tcp_socket.h
@@ -4,24 +4,33 @@
 #include <vector>
 #include <string>
 #include "port.h"
+#include "socket_interface.h"
 
-class tcp_socket
+class tcp_socket : public socket_interface
 {
 public:
     tcp_socket(socket_t sock, const char_ptr ip, ushort port);
     virtual ~tcp_socket();
-
+    /*
     virtual int send(const char_ptr buffer, int len, int pos = 0);
     virtual int send(const std::string& msg);
+    */
+    int send(char_ptr buff, int lenth) override;
 
-    virtual int recv(char *buffer, int len, int pos = 0);
+    /* virtual int recv(char *buffer, int len, int pos = 0);
     virtual int recv(std::string& msg);
-    virtual int recv(vector_char& buffer);
+    virtual int recv(vector_char& buffer); */
 
-    const std::string& remote_ip();
-    const ushort remote_port();
+    int recv(char_ptr buff, int length) override;
 
-    void close();
+//     const std::string& remote_ip();
+//     const ushort remote_port();
+
+    void close() override;
+
+    std::string& remote_ip() override;
+
+    ushort& remote_port() override;
 
 protected:
     std::string _remote_ip;
