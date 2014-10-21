132efb4576be2540a34a569aa658c3390dc258bb
Bình Nguyên <crziter@gmail.com>
2014-10-21 09:21:51 +0700

    Thêm xử lí lỗi 400 (bad request)

--- httpd/http_error.cpp
+++ httpd/http_error.cpp
@@ -1,9 +1,17 @@
 #include "http_error.h"
+#include "file_operations.h"
+#include "port.h"
 
 std::string http_error::error(http_status status)
 {
     switch (status) {
     case BAD_REQUEST:
+        {
+            std::string content;
+            std::string err404(ERROR_400);
+            file_operations::get_content_jail(err404, content);
+            return content;
+        }
         break;
     }
 
--- httpd/http_error.h
+++ httpd/http_error.h
@@ -2,13 +2,12 @@
 #define __http_error_h__
 
 #include <iostream>
-#include "file_operations.h"
 #include "http_specs.h"
 
 class http_error
 {
 public:
-    std::string error(http_status status);
+    static std::string error(http_status status);
 };
 
 #endif
