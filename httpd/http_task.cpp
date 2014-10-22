3578382227cc25ead06a6bf30ceaa8331172f16e
Bình Nguyên <crziter@gmail.com>
2014-10-22 15:57:32 +0700

    Chỉnh lại logic

--- httpd/http_task.cpp
+++ httpd/http_task.cpp
@@ -1,5 +1,6 @@
 #include "http_task.h"
 #include "http_error.h"
+#include "content_service.h"
 
 http_task::http_task(http_request& rq, http_response& rp, configuration& conf)
     :_request(rq), _response(rp), _conf(conf)
@@ -9,7 +10,7 @@ http_task::http_task(http_request& rq, http_response& rp, configuration& conf)
 void http_task::run()
 {
     IF_DEBUG({
-        std::cout << "Handle " << _request.method_str() << " request to " << _request.uri().uri() << std::endl;
+        std::cout << _request.method_str() << " " << _request.uri().path() << "(" << _request.uri().query() << ")" << std::endl;
     });
 
     _response.version(_request.version());
@@ -17,10 +18,14 @@ void http_task::run()
     host_info* host = _conf.config_for_host(_request.host());
 
     if (host != nullptr) {
-        std::string found = "Found :)";
-        _response.content(found);
-    }
-    else {
+        content_service cs(_request, _conf);
+        cs.process(_response);
+
+//         _response.status(status);
+
+        _response.content(cs.content());
+    } else {
+        _response.status(http_status::OK);
         _response.content(http_error::error(http_status::BAD_REQUEST));
         _response.header().append(http_header_str(header_list::CONTENT_TYPE), content_type_str(content_type::TEXT_HTML));
     }
