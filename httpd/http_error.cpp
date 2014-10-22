8b702c06076ec91bc8d3aca60743f4a493dd3d2f
Bình Nguyên <crziter@gmail.com>
2014-10-22 15:54:35 +0700

    Thêm xử lí lỗi 404

--- httpd/http_error.cpp
+++ httpd/http_error.cpp
@@ -4,17 +4,24 @@
 
 std::string http_error::error(http_status status)
 {
+    std::string content;
+
     switch (status) {
     case BAD_REQUEST:
         {
-            std::string content;
-            std::string err404(ERROR_400);
+            std::string err400(ERROR_400);
+            file_operations::get_content_jail(err400, content);
+        }
+        break;
+
+    case NOT_FOUND:
+        {
+            std::string err404(ERROR_404);
             file_operations::get_content_jail(err404, content);
-            return content;
         }
         break;
     }
 
-    return "";
+    return content;
 }
 
